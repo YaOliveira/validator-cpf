a119ca5663175a55335ade09ecf0f9d58b6a3b93
YaOliveira <magicsunshine_ya@msn.com>
2019-05-07 16:57:53 -0300

    passei o eslint e inclusão do mocha no eslint.js

--- test/index.spec.js
+++ test/index.spec.js
@@ -2,37 +2,35 @@ const chai = require('chai');
 const expect = chai.expect;
 const validatorCPF = require('../index');
 
-
 describe('função validatorCPF', () => {
-    it('103.617.448-46 número de CPF é válido', () => {
-        expect(validatorCPF.validatorCPF('103.617.448-46')).to.equal(true);
-    });
+  it('103.617.448-46 número de CPF é válido', () => {
+    expect(validatorCPF.validatorCPF('103.617.448-46')).to.equal(true);
+  });
 
-    it('10361744846 CPF em formato string é válido', () => {
-        expect(validatorCPF.validatorCPF('10361744846')).to.equal(true);
-    });
+  it('10361744846 CPF em formato string é válido', () => {
+    expect(validatorCPF.validatorCPF('10361744846')).to.equal(true);
+  });
 
-    it('10361744846 número de CPF é válido', () => {
-        expect(validatorCPF.validatorCPF(10361744846)).to.equal(true);
-    });
+  it('10361744846 número de CPF é válido', () => {
+    expect(validatorCPF.validatorCPF(10361744846)).to.equal(true);
+  });
 
-    it('10361744847 não tem o segundo dígito válido', () => {
-        expect(validatorCPF.validatorCPF('10361744847')).to.equal(false);
-    });
+  it('10361744847 não tem o segundo dígito válido', () => {
+    expect(validatorCPF.validatorCPF('10361744847')).to.equal(false);
+  });
 
-    it('10361744856 não tem o primeiro dígito válido', () => {
-        expect(validatorCPF.validatorCPF('10361744856')).to.equal(false);
-    });
+  it('10361744856 não tem o primeiro dígito válido', () => {
+    expect(validatorCPF.validatorCPF('10361744856')).to.equal(false);
+  });
 
-    it('1036174485 falta 1 número', () => {
-        expect(validatorCPF.validatorCPF('1036174485')).to.equal(false);
-    });
+  it('1036174485 falta 1 número', () => {
+    expect(validatorCPF.validatorCPF('1036174485')).to.equal(false);
+  });
 
-    it('103617448466 tem 1 número a mais', () => {
-        expect(validatorCPF.validatorCPF('103617448466')).to.equal(false);
-    });
+  it('103617448466 tem 1 número a mais', () => {
+    expect(validatorCPF.validatorCPF('103617448466')).to.equal(false);
+  });
 });
-
 
 // const chai = require('chai');
 // const expect = chai.expect;
@@ -46,7 +44,6 @@ describe('função validatorCPF', () => {
 //     it('cpf valido', () => {
 //         expect(validatorCPF.cpfValidator('21973151880')).to.equal(true);
 //     });
-
 
 // })
 

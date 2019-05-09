634bef2de8e74b48415c35cd80816277477b9128
YaOliveira <magicsunshine_ya@msn.com>
2019-05-09 10:21:42 -0300

    function digit

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
-function digit(arr, multiplier) {
-    const algorithm = arr.reduce((result, num) => result + (num * multiplier--), 0) % 11;
-    return algorithm < 2 ? 0 : 11 - algorithm;
+// function digit(arr, multiplier) {
+//     const algorithm = arr.reduce((result, num) => result + (num * multiplier--), 0) % 11;
+//     return algorithm < 2 ? 0 : 11 - algorithm;
+// }
+
+function digit(array, multiplier) {
+    const mult = array.map((num) => num * multiplier);
+    const soma = mult.reduce((soma, num) => soma = soma + num);
+    if ((soma % 11) < 2) {
+        return 0
+    }
+    else {
+        return 11 - (soma % 11)
+    }
 }
 
 function validatorCPF(string) {
-    
+
     let arrCPFNumber = string
         .toString()
         .replace(/\D/g, '')
         .split('')
         .map(Number);
-        
+
     const isEqualNumber = arrCPFNumber.every((elem) => elem === arrCPFNumber[0]);
     const multiplyOne = 10;
     const multiplyTwo = 11;
